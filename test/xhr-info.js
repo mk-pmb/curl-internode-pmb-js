25b73161b6129427c6b9a92e22d5fcf5ea564809
mk-pmb <mk-pmb@users.noreply.github.com>
2017-02-02 15:35:03 +0100

    add test names and HTTPS link dynamically

---
+++ test/xhr-info.js
@@ -0,0 +1,15 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, browser: true */
+/* -*- tab-width: 2 -*- */
+(function () {
+  'use strict';
+  var body = document.getElementsByTagName('body')[0],
+    testName = location.pathname.replace(/^\S+\//, '').replace(/\.html$/, '');
+  function mktag(tn) { return document.createElement(tn); }
+  body.appendChild(mktag('h2')).innerHTML = testName;
+  if (location.protocol === 'file:') {
+    body.appendChild(mktag('p')).innerHTML = 'XHR errors' +
+      ' when testing via file:// protocol? Try <a href="' +
+      'https://mk-pmb.github.io/curl-internode-pmb-js/test/' + testName +
+      '.html">HTTP(S)</a>.';
+  }
+}());
